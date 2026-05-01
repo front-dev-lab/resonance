df9cf08976a76d07581741d5bcf9417671640398
Taras <taras@coderiver.com.ua>
2026-05-01 12:51:52 +0300

    small changes

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,14 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: "/resonance",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "tech-specs": resolve(__dirname, 'tech-specs.html'),
+        compare: resolve(__dirname, 'compare.html'),
+      },
+    },
+  },
 });
